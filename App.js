933f70e018a5e2e8ba4f6feb3f076999b6b1eb27
Neimerc <Neil_Mercury@yahoo.es>
2021-03-17 20:52:50 +0100

    Stylesheet objects

--- App.js
+++ App.js
@@ -3,23 +3,9 @@ import { Button, StyleSheet, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <TextInput
-          placeholder="Course Goal"
-          style={{
-            width: '80%',
-            borderColor: 'black',
-            borderWidth: 1,
-            padding: 10,
-          }}
-        />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input} />
         <Button title="ADD" />
       </View>
     </View>
@@ -29,4 +15,19 @@ export default function App() {
 // A React Native app is all about using components and then adding the right layout to
 // the components.
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+  },
+});
